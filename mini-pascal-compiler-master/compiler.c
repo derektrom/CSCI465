2c1cb8b7e6587df1c7f13ce595674446c2ca5fd1
derektrom <54723488+derektrom@users.noreply.github.com>
2020-11-17 20:59:42 -0600

    adds c version

---
+++ mini-pascal-compiler-master/compiler.c
@@ -0,0 +1,120 @@
+#include "compiler.h"
+
+#ifndef YYCOMPILE
+
+# include "tokens.h"
+# include "scanner.h"
+# include "parser.h"
+# include "symtab.h"
+# include "io.h"
+# include "ast.h"
+# include "icg.h"
+
+#endif /* YYCOMPILE */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#ifdef YYCOMPILE
+extern FILE *yyin;
+extern int yylex(void);
+#else
+extern int pcscanerrors;
+#endif /* YYCOMPILE */
+
+void
+usage(const char *progname) {
+    printf("Usage: %s filename\n  filename\tPascal file to compile\n", progname);
+}
+
+char *
+strtolower(char *s) {
+    char *c = s;
+    for (; *c; ++c) {
+        *c = tolower(*c);
+    }
+
+    return s;
+}
+
+int
+main(int argc, char **argv) {
+    FILE *fp;
+    char *filename;
+#ifdef YYCOMPILE
+    int token;
+#else
+    pctoken *token;
+    pctoken *nexttoken;
+#endif /* YYCOMPILE */
+
+    /* read the filename from command line */
+    if (argc > 1) {
+        filename = argv[1];
+
+        /* open our file */
+        fp = fopen(filename, "r");
+        if (!fp) {
+            printf("Unable to open file: %s.\n", filename);
+            return EXIT_FAILURE;
+        }
+
+#ifdef YYCOMPILE
+        yyin = fp;
+#endif
+    } else {
+        usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    printf("Reading file: %s\n\n", filename);
+
+    /** just run the lexer for now, skipping the scanner */
+#ifdef YYCOMPILE
+    while ((token = yylex())) {
+        //printf("%d\n", token);
+    }
+#else
+    /* initialize our symbol table */
+    pcintializesymtab();
+
+    if (pcparse(fp)) {
+        printf("\nPARSING COMPLETED SUCCESSFULLY!!!!!\n");
+    } else {
+        printf("\nERRORS PARSING!!!!!\n");
+        pcscanerrors = 1;
+    }
+
+    /* spit out errors */
+    if (pcscanerrors) {
+        printf("\n%d ERRORS during scanning!\n", pcscanerrors);
+    }
+
+    /* save our AST tree */
+    FILE *astfp;
+    if ((astfp = fopen("astfp.txt", "w"))) {
+        AST_print(astroot, astfp);
+        printf("\nSaved astfp.txt\n");
+    }
+
+    /* save our output */
+    FILE *output;
+    if (!(output = fopen("output.s", "w"))) {
+        printf("\nUNABLE TO SAVE\n");
+        pccleanupsymtab();
+        return EXIT_FAILURE;
+    }
+
+    /* print our symbol table */
+    pcprintsymtab();
+
+    pcicg_start(output);
+    AST_cleanup(&astroot);
+
+    /* print our symbol table */
+    pccleanupsymtab();
+#endif /* YYCOMPILE */
+
+    return EXIT_SUCCESS;
+}
---
+++ mini-pascal-compiler-master/tests/validinput.pas
@@ -0,0 +1,1 @@
+program example;var x,y:integer;function gcd(a,b:integer):integer;begin {gcd}if b=0 then gcd:=a else gcd:=(b,a mod b)end; {gcd}begin{example}read(x,y);write(gcd(x,y));end.
